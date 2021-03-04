31a2d8cdfee340f3b73b0c6d1ac43eb9b71b1478
yujinnnnnnnnn <79399429+yujinnnnnnnnn@users.noreply.github.com>
2021-03-05 00:14:40 +0900

    웹스터디 1주차
    자기소개 페이지 만들기

---
+++ main.js
@@ -0,0 +1,12 @@
+const toggleBtn = document.querySelector('.navbar_toogleBtn');
+const menu=document.querySelector('.navbar_menu');
+const icons=document.querySelector('.nabar_icons');
+
+toggleBtn.addEventListener('click', ( ) => { 
+    menu.classlist.toggle('active');
+    icons.classList.toggle('active');
+});
+
+.photo.jpg {
+
+}
